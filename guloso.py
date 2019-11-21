ff5c5e280cdee3c786d7e2b133b18a6d769bd972
Mateus Gundel <mateus8923@gmail.com>
2019-11-21 00:50:38 -0300

    [FIX] Arrumado identação do código

--- guloso.py
+++ guloso.py
@@ -1,7 +1,7 @@
 text_file = open("arquivo_entrada_binpacking.txt")
 
 matrix = []
-i=0
+i = 0
 
 for line in text_file:
     if " u" in line:
@@ -18,37 +18,36 @@ for line in text_file:
             matrix[i].append(line)
         else:
             matrix[i].append(int(line))
-            
-capacity            = 0
-number_of_items     = 0
-best_know_solution  = 0
-current_weight      = 0
-bin_counter         = 0
+
+capacity = 0
+number_of_items = 0
+best_know_solution = 0
+current_weight = 0
+bin_counter = 0
 
 for problem in matrix:
-    capacity            = int(problem[1][0])
-    number_of_items     = int(problem[1][1])
-    best_know_solution  = int(problem[1][2])
-    bin_counter         = 0
-    #print(capacity,number_of_items,best_know_solution)
-    aux_list            = problem[2:]
+    capacity = int(problem[1][0])
+    number_of_items = int(problem[1][1])
+    best_know_solution = int(problem[1][2])
+    bin_counter = 0
+    # print(capacity,number_of_items,best_know_solution)
+    aux_list = problem[2:]
     aux_list.sort()
-    current_weight      = 0
-    i                   = 1
-    teste               = 0
+    current_weight = 0
+    i = 1
+    teste = 0
     while len(aux_list) > 0:
         if aux_list[-i] + current_weight <= capacity:
-            current_weight  += aux_list[-i]
+            current_weight += aux_list[-i]
             aux_list.pop(-i)
-            i               = 1
+            i = 1
         else:
             i += 1
         if (
-            i >= len(aux_list) or 
-            current_weight == capacity
+                i >= len(aux_list) or
+                current_weight == capacity
         ) and len(aux_list) > 0:
-            current_weight  = 0
-            i               = 1
-            bin_counter     += 1
+            current_weight = 0
+            i = 1
+            bin_counter += 1
     print(f"Foi possível encher {bin_counter} bins no problema {problem[0]} usando o algoritmo guloso.")
-

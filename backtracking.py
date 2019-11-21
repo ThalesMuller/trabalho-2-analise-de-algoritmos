776fdae680bd28b21250b303767ddfd61e97a163
ThalesMuller <thalesem@mx2.unisc.br>
2019-11-20 23:08:52 -0300

    [ADD] adding bin packing input file

--- backtracking.py
+++ backtracking.py
@@ -3,4 +3,4 @@ from util import Utils
 
 class CalculoBacktracking:
     def __init__(self):
-        self.entrada = Utils.ler_entrada()
+        self.entrada = Utils.ler_entrada("arquivo_entrada_mochila.json")

e0b636b5365fe17f5fbc9f683e57d7d0fa1eeba0
Mateus Gundel <mateus8923@gmail.com>
2019-11-27 09:09:49 -0300

    [FIX] Corrigido método para fazer leitura do arquivo

--- programa-2-backtrack/util.py
+++ programa-2-backtrack/util.py
@@ -5,4 +5,10 @@ class Utils:
     @staticmethod
     def ler_entrada(file_name):
         with open(file_name, 'r') as file:
-            return json.load(file)
+            array = []
+            for item in json.load(file):
+                if not item['item'] == 'knapsack':
+                    array.append((item['item'], item['weight'], item['value']))
+            return tuple(array)
+
+# print(os.system("ls"))
